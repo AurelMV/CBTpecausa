ca615057759b143c868d159643512aff21985679
Marco Valdez <marcotuamortlv@gmail.com>
2024-07-24 22:39:14 -0500

    algo de diseño nomas

--- CapaPresentacion/formUsuario.Designer.cs
+++ CapaPresentacion/formUsuario.Designer.cs
@@ -149,6 +149,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(11, 230);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
